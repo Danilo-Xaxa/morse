31d87650bd0c0cd931028a08fb0bd90eedff4f7b
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-21 13:21:11 -0300

    tradutor como classe
    Agora o Tradutor será uma classe no módulo tradutor.py, contendo os dois dicionários e os dois class methods

---
+++ testador.py
@@ -0,0 +1,12 @@
+from tradutor import Tradutor as t
+
+
+def main():
+    exemplo_texto = 'CESAR E DANILO'
+    print(t.encriptar(exemplo_texto))
+
+    exemplo_morse = '-.-. . ... .- .-. / . / -.. .- -. .. .-.. ---'
+    print(t.decriptar(exemplo_morse))
+
+if __name__ == '__main__':
+    main()
--- tradutor.py
+++ tradutor.py
@@ -1,16 +1,19 @@
-TEXTO_PARA_MORSE = {'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.', '0':'-----', ', ':'--..--', '.':'.-.-.-', '?':'..--..', '/':'-..-.', '-':'-....-', '(':'-.--.', ')':'-.--.-', ' ': '/'}
+class Tradutor:
+    TEXTO_PARA_MORSE = {'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....','7':'--...', '8':'---..', '9':'----.', '0':'-----', ', ':'--..--', '.':'.-.-.-', '?':'..--..', '/':'-..-.', '-':'-....-', '(':'-.--.', ')':'-.--.-', ' ': '/'}
 
-MORSE_PARA_TEXTO = {v: k for k, v in TEXTO_PARA_MORSE.items()}
+    MORSE_PARA_TEXTO = {v: k for k, v in TEXTO_PARA_MORSE.items()}
 
-def encriptar(texto):
-    '''
-    Recebe uma string de texto e retorna a string traduzida para código morse
-    '''
-    return ''.join(TEXTO_PARA_MORSE[caractere.upper()] + ' ' for caractere in texto)
+    @classmethod
+    def encriptar(cls, texto):
+        '''
+        Recebe uma string de texto e retorna a string traduzida para código morse
+        '''
+        return ''.join(cls.TEXTO_PARA_MORSE[caractere.upper()] + ' ' for caractere in texto)
 
-def decriptar(morse):
-    '''
-    Recebe uma string de código morse e retorna a string traduzida para texto
-    '''
-    return ''.join(MORSE_PARA_TEXTO[caractere.upper()] for caractere in morse.split(' '))
-    
+    @classmethod
+    def decriptar(cls, morse):
+        '''
+        Recebe uma string de código morse e retorna a string traduzida para texto
+        '''
+        return ''.join(cls.MORSE_PARA_TEXTO[caractere.upper()] for caractere in morse.split(' '))
+        
