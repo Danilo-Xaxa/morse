a2056ccab2e3b47c90aa0b55a0515a59d435182d
Danilo Xaxa <daniloxaxa01@gmail.com>
2021-12-21 14:54:44 -0300

    criando model e fazendo migrações

--- projeto_morse/app_principal/models.py
+++ projeto_morse/app_principal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Traducao(models.Model):
+    morse = models.CharField(max_length=500)
+    texto = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.morse + ' -> ' + self.texto
